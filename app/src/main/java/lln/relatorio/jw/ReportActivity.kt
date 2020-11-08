220a9a4353e01410effc7d4bb9ba37302ca764ad
Lucas Lima T <lukaslymauchiha@gmail.com>
2020-11-08 17:05:09 -0300

    Initial commit

---
+++ app/src/main/java/lln/relatorio/jw/ReportActivity.kt
@@ -0,0 +1,11 @@
+package lln.relatorio.jw
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ReportActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_report)
+    }
+}
